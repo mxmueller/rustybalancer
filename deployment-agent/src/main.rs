5f75240549852d5b3bad251b2bf823dbfd0830a7
Maximilian Müller <maximilian.mm.mueller@mercedes-benz.com>
2024-07-16 15:10:32 +0200

    #5_Auto-generate-Containers-with-docker-swarm-in-LB

--- deployment-agent/src/main.rs
+++ deployment-agent/src/main.rs
@@ -1,6 +1,25 @@
 mod socket;
+mod swarm;
+mod stats;
+
+use bollard::Docker;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
-    socket::socket().await;
+    if let Err(e) = swarm::swarm_boot().await {
+        eprintln!("Error: {}", e);
+    } else {
+        let docker = Docker::connect_with_unix_defaults().expect("Failed to connect to Docker");
+
+        match stats::get_all_container_stats(&docker).await {
+            Ok(stats_map) => {
+                for (id, stats) in stats_map {
+                    println!("Container ID: {}", id);
+                    println!("{:#?}", stats);
+                }
+            }
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+    //socket::socket().await;
 }
