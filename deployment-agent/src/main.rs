f5212d3004d04fc91175ac3ddb61edb28eb92669
Maximilian Müller <maximilian.mm.mueller@mercedes-benz.com>
2024-07-18 11:01:09 +0200

    #5_Get-stats-from-swarm-containers

--- deployment-agent/src/main.rs
+++ deployment-agent/src/main.rs
@@ -2,24 +2,19 @@ mod socket;
 mod swarm;
 mod stats;
 
-use bollard::Docker;
-
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
     if let Err(e) = swarm::swarm_boot().await {
         eprintln!("Error: {}", e);
-    } else {
-        let docker = Docker::connect_with_unix_defaults().expect("Failed to connect to Docker");
-
-        match stats::get_all_container_stats(&docker).await {
-            Ok(stats_map) => {
-                for (id, stats) in stats_map {
-                    println!("Container ID: {}", id);
-                    println!("{:#?}", stats);
-                }
-            }
-            Err(e) => eprintln!("Error: {}", e),
-        }
     }
+
+    println!("FOOOO");
+
+    if let Err(e) = stats::display_stats().await {
+        eprintln!("Error: {}", e);
+    } else {
+        println!("FOOOO");
+    }
+
     //socket::socket().await;
 }
