44c8e7bd06312993b94964fee8a81d78d784244e
Maximilian Müller <63018152+mxmueller@users.noreply.github.com>
2024-09-23 17:53:24 +0200

    fix: setup.sh

--- jobs/setup.sh
+++ jobs/setup.sh
@@ -2,16 +2,20 @@
 
 ENV_FILE_PATH="../.env"
 
+# for testing
+# "mxmller/rustybalancer-playground:latest"
+# 5000
+
 # Default values
 DEFAULT_DOCKER_HOST="unix:///var/run/docker.sock"
 DEFAULT_DOCKER_SOCKET_VOLUME="/var/run/docker.sock"
-DEFAULT_DOCKER_IMAGE="mxmller/rustybalancer-playground:latest"
+DEFAULT_DOCKER_IMAGE=""
 DEFAULT_HOST_PORT_WS_DEPLOYMENT_AGENT="2547"
 DEFAULT_HOST_PORT_WS_BALANCER="2551"
 DEFAULT_HOST_PORT_HTTP_DEPLOYMENT_AGENT="2550"
 DEFAULT_HOST_PORT_HTTP_BALANCER="2548"
 DEFAULT_PORT_DASHBOARD="8501"
-DEFAULT_TARGET_PORT="5000"
+DEFAULT_TARGET_PORT=""
 DEFAULT_DEFAULT_CONTAINER="5"
 DEFAULT_APP_IDENTIFIER="RUSTYBALANCER"
 DEFAULT_REDIS_PORT="6379"
@@ -37,10 +41,13 @@ DEFAULT_CACHE_CAPACITY="10000"
 # Function to create .env-file
 create_env_file() {
   cat <<EOF > "$ENV_FILE_PATH"
+# Worker
+DOCKER_IMAGE=$3
+TARGET_PORT=$9
+
 # Docker Configuration
 DOCKER_HOST=$1
 DOCKER_SOCKET_VOLUME=$2
-DOCKER_IMAGE=$3
 
 # Port Configuration
 # WebSocket Ports
@@ -53,9 +60,6 @@ HOST_PORT_HTTP_BALANCER=$7
 
 # Dashboard Port
 PORT_DASHBOARD=$8
-
-# Target Port
-TARGET_PORT=$9
 
 # Application Configuration
 DEFAULT_CONTAINER=${10}
@@ -95,8 +99,8 @@ EOF
 # Choosing either expert or basic version for setup
 echo "This script will generate your .env-file."
 echo "Which version do you want to use:"
-echo "1) Basic version (default values)"
-echo "2) Expert version (you can choose your own values)"
+echo "1) Plug-and-Play (Recommended)"
+echo "2) Advanced Setup (Developers)"
 
 read -p "Your choice: " choice
 
